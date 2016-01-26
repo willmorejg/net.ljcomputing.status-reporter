edd90bb6d1117a4b57e82c74762f0837f981ccc3
James G. Willmore <willmorejg@gmail.com>
2016-01-26 03:26:02 -0500

    Modified POM to correct issue with Javadoc generation (Javadoc location for Hibernate Validator Javadoc (5.1.3.Final) was incorrect, causing UMLGraph to fail).

--- src/test/java/net/ljcomputing/core/test/ExpectedMockResults.java
+++ src/test/java/net/ljcomputing/core/test/ExpectedMockResults.java
@@ -23,21 +23,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * @author James G. Willmore
+ * Class for the expected mocked results.
  *
+ * @author James G. Willmore
  */
 public class ExpectedMockResults {
+  
+  /** The gson service. */
   private GsonConverterService gsonService;
 
+  /** The url. */
   private String url;
+  
+  /** The expected path. */
   private String expectedPath;
+  
+  /** The expected value. */
   private String expectedValue;
+  
+  /** The posted request body. */
   private Domain postedRequestBody;
+  
+  /** The posted path variable. */
   private String postedPathVariable;
 
+  /**
+   * Instantiates a new expected mock results.
+   */
   ExpectedMockResults() {
   }
 
+  /**
+   * Instantiates a new expected mock results.
+   *
+   * @param builder the builder
+   */
   private ExpectedMockResults(Builder builder) {
     this.gsonService = builder.gsonService;
     this.url = builder.url;
@@ -47,6 +67,11 @@ public class ExpectedMockResults {
     this.postedPathVariable = builder.postedPathVariable;
   }
 
+  /**
+   * Json payload.
+   *
+   * @return the string
+   */
   private String jsonPayload() {
     if (null != postedRequestBody) {
       return gsonService.toJson(postedRequestBody);
@@ -55,32 +80,68 @@ public class ExpectedMockResults {
     return "";
   }
 
+  /**
+   * Json payload.
+   *
+   * @param json the json
+   * @param klass the klass
+   */
   private void jsonPayload(String json, Class<? extends Domain> klass) {
     if (null != json) {
       postedRequestBody = (Domain) gsonService.fromJson(json, klass);
     }
   }
 
+  /**
+   * Gets the url.
+   *
+   * @return the url
+   */
   public String getUrl() {
     return url;
   }
 
+  /**
+   * Gets the expected path.
+   *
+   * @return the expected path
+   */
   public String getExpectedPath() {
     return expectedPath;
   }
 
+  /**
+   * Gets the expected value.
+   *
+   * @return the expected value
+   */
   public String getExpectedValue() {
     return expectedValue;
   }
 
+  /**
+   * Gets the posted request body.
+   *
+   * @return the posted request body
+   */
   public Domain getPostedRequestBody() {
     return postedRequestBody;
   }
 
+  /**
+   * Update posted request body.
+   *
+   * @param json the json
+   */
   public void updatePostedRequestBody(String json) {
     jsonPayload(json, postedRequestBody.getClass());
   }
 
+  /**
+   * Gets the domain uuid.
+   *
+   * @return the domain uuid
+   */
   public String getDomainUuid() {
     if (null != postedRequestBody && null != postedRequestBody.getUuid()
         && !postedRequestBody.getUuid().trim().equals("")) {
@@ -90,14 +151,27 @@ public class ExpectedMockResults {
     return "";
   }
 
+  /**
+   * Gets the posted path variable.
+   *
+   * @return the posted path variable
+   */
   public String getPostedPathVariable() {
     return postedPathVariable;
   }
 
+  /**
+   * Gets the json payload.
+   *
+   * @return the json payload
+   */
   public String getJsonPayload() {
     return jsonPayload();
   }
 
+  /**
+   * @see java.lang.Object#toString()
+   */
   @Override
   public String toString() {
     return "ExpectedMockResults [url=" + url + ", expectedPath=" + expectedPath
@@ -106,42 +180,91 @@ public class ExpectedMockResults {
         + ", gsonService=" + gsonService + "]";
   }
 
+  /**
+   * Builder for the expected mocked results.
+   */
   @Component
   public static class Builder {
+    
+    /** The gson service. */
     @Autowired
     private GsonConverterService gsonService;
 
+    /** The url. */
     private String url;
+    
+    /** The expected path. */
     private String expectedPath;
+    
+    /** The expected value. */
     private String expectedValue;
+    
+    /** The posted request body. */
     private Domain postedRequestBody;
+    
+    /** The posted path variable. */
     private String postedPathVariable;
 
+    /**
+     * Url.
+     *
+     * @param url the url
+     * @return the builder
+     */
     public Builder url(String url) {
       this.url = url;
       return this;
     }
 
+    /**
+     * Expected path.
+     *
+     * @param expectedPath the expected path
+     * @return the builder
+     */
     public Builder expectedPath(String expectedPath) {
       this.expectedPath = expectedPath;
       return this;
     }
 
+    /**
+     * Expected value.
+     *
+     * @param expectedValue the expected value
+     * @return the builder
+     */
     public Builder expectedValue(String expectedValue) {
       this.expectedValue = expectedValue;
       return this;
     }
 
+    /**
+     * Posted request body.
+     *
+     * @param postedRequestBody the posted request body
+     * @return the builder
+     */
     public Builder postedRequestBody(Domain postedRequestBody) {
       this.postedRequestBody = postedRequestBody;
       return this;
     }
 
+    /**
+     * Posted path variable.
+     *
+     * @param postedPathVariable the posted path variable
+     * @return the builder
+     */
     public Builder postedPathVariable(String postedPathVariable) {
       this.postedPathVariable = postedPathVariable;
       return this;
     }
 
+    /**
+     * Builds the expected mock results.
+     *
+     * @return the expected mock results
+     */
     public ExpectedMockResults build() {
       return new ExpectedMockResults(this);
     }
