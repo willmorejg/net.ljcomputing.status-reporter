263a43adfa36bbafacb91c5d002c8e6c2d695fb2
James G. Willmore <willmorejg@gmail.com>
2016-02-09 05:36:12 -0500

    Modified styling. Added angular aside module.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -8,6 +8,7 @@
     , 'ui.bootstrap.datepicker'
     , 'ui.bootstrap.tooltip'
     , 'angularUtils.directives.dirPagination'
+    , 'ngAside'
     , 'dm.stickyNav'
     , 'ui.tree'
     , 'ui.router'
--- src/main/resources/static/js/status-reporter/sr.js
+++ src/main/resources/static/js/status-reporter/sr.js
@@ -110,4 +110,39 @@
 
     return me;
   });
+  
+  sr.controller('asideCtrl', function($scope, $aside) {
+    
+    $scope.asideState = {
+      open: false
+    };
+    
+    $scope.openAside = function(position, backdrop) {
+      $scope.asideState = {
+        open: true,
+        position: position
+      };
+      
+      function postClose() {
+        $scope.asideState.open = false;
+      }
+      
+      $aside.open({
+        templateUrl: 'js/status-reporter/mainMenu.htm',
+        placement: position,
+        size: 'sm',
+        backdrop: backdrop,
+        controller: function($scope, $uibModalInstance) {
+          $scope.ok = function(e) {
+            $uibModalInstance.close();
+            e.stopPropagation();
+          };
+          $scope.cancel = function(e) {
+            $uibModalInstance.dismiss();
+            e.stopPropagation();
+          };
+        }
+      }).result.then(postClose, postClose);
+    }
+  });
 })();
